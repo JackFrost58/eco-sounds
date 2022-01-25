4619282755282b1a23767641b36052667a8fd16f
JackFrost58 <zgv2324@gmail.com>
2022-01-25 16:36:18 +0300

    feat: swicher languages for portfolio

--- portfolio/js/helper.js
+++ portfolio/js/helper.js
@@ -1,15 +1,12 @@
 import i18Obj from "./translate.js";
-console.log(i18Obj)
 
 function getTranslate(language) {
     const items = document.querySelectorAll('[data-i18]');
-
-    //if (language === 'ru') {
-        items.forEach((element) => {
-            console.log(i18Obj.language)
-          //element.innerHTML = i18Obj.language.element.dataset.i18;
-        })
-    //}
+    items.forEach((element) => {
+        element.innerHTML = i18Obj[language][element.dataset.i18]
+        if(element.pla);
+    })
+   
 }
 
 export default getTranslate
--- portfolio/js/index.js
+++ portfolio/js/index.js
@@ -1,23 +1,36 @@
 import i18Obj from './translate.js';
 import getTranslate from './helper.js';
 
-const navLinks = document.querySelector(".header-container__menu");
-const hamburger = document.querySelector(".hamburger");
-const hamburgerElements = document.querySelectorAll(".hamburger__line");
+const navLinks = document.querySelector('.header-container__menu');
+const hamburger = document.querySelector('.hamburger');
+const hamburgerElements = document.querySelectorAll('.hamburger__line');
+const buttonSwitch = document.querySelector('.header-container-switch')
 
 
-hamburger.addEventListener("click", closeMenu);
-navLinks.addEventListener("click", closeMenu);
 
+hamburger.addEventListener('click', closeMenu);
+navLinks.addEventListener('click', closeMenu);
+buttonSwitch.addEventListener('click', changeLang)
+
+function changeLang(event){
+    const activeButton = document.querySelector('.header-container-switch__button_checked');
+   
+    if(!event.target.classList.contains('header-container-switch__button_checked')){
+        event.target.classList.add('header-container-switch__button_checked');
+        activeButton.classList.remove('header-container-switch__button_checked');
+    } 
+
+    getTranslate(event.target.innerHTML);
+}
 
 function closeMenu(event) {
-    navLinks.classList.toggle("header-container__menu-active");
+    navLinks.classList.toggle('header-container__menu-active');
     hamburgerElements.forEach((element) => {
         element.classList.toggle('active');
     })
 }
 
-getTranslate('ru');
+
 
 
 //console.log(" Оценка: 85/75 \n 1. Верстка соответствует макету +48 \n 2. Ни на одном из разрешений до 320px включительно не появляется горизонтальная полоса прокрутки +15 \n 3. На ширине экрана 768рх и меньше реализовано адаптивное меню +22")
