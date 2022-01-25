b2393a1639859e7a860a79e48e9170699480eb50
JackFrost58 <zgv2324@gmail.com>
2022-01-25 17:04:01 +0300

    fix: style for ru language

--- portfolio/js/helper.js
+++ portfolio/js/helper.js
@@ -3,8 +3,13 @@ import i18Obj from "./translate.js";
 function getTranslate(language) {
     const items = document.querySelectorAll('[data-i18]');
     items.forEach((element) => {
-        element.innerHTML = i18Obj[language][element.dataset.i18]
-        if(element.pla);
+        let text = i18Obj[language][element.dataset.i18];
+        if(element.placeholder) {
+            element.placeholder = text;
+            element.textContent = '';
+        } else {
+            element.innerHTML = text;
+        } 
     })
    
 }
--- portfolio/js/translate.js
+++ portfolio/js/translate.js
@@ -37,7 +37,10 @@ const i18Obj = {
       'price-description-3-span-5': 'Make up, visage, hairstyle',
       'order': 'Order shooting',
       'contact-me': 'Contact me',
-      'send-message': 'Send message'
+      'send-message': 'Send message',
+      'email': 'Email',
+      'phone': 'Phone',
+      'message-area': 'Message'
     },
     'ru': {
       'skills': 'Навыки',
@@ -77,7 +80,10 @@ const i18Obj = {
       'price-description-3-span-5': 'Макияж, визаж, прическа',
       'order': 'Заказать съемку',
       'contact-me': 'Свяжитесь со мной',
-      'send-message': 'Отправить'
+      'send-message': 'Отправить',
+      'email': 'Электронная почта',
+      'phone': 'Телефон',
+      'message-area': 'Сообщение'
     }
   }
 export default i18Obj;
