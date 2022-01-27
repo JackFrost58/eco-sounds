511b7868d025b3cb368fefadd4cc4feecf152d6d
JackFrost58 <zgv2324@gmail.com>
2022-01-27 19:33:43 +0300

    feat: add button switch theme

--- portfolio/js/helper.js
+++ portfolio/js/helper.js
@@ -1,4 +1,4 @@
-import i18Obj from "./translate.js";
+import {i18Obj, LIST_CHANGE_BG_COLOR } from "./translate.js";
 
 function getTranslate(language) {
     const items = document.querySelectorAll('[data-i18]');
@@ -10,8 +10,50 @@ function getTranslate(language) {
         } else {
             element.innerHTML = text;
         } 
-    })
-   
+    })  
 }
 
-export default getTranslate
+function changeLang(event){
+    const activeButton = document.querySelector('.header-container-switch__button_checked');
+   
+    if(!event.target.classList.contains('header-container-switch__button_checked')){
+        event.target.classList.add('header-container-switch__button_checked');
+        activeButton.classList.remove('header-container-switch__button_checked');
+    } 
+
+    getTranslate(event.target.innerHTML);
+}
+
+function closeMenu(event) {
+    const hamburgerElements = document.querySelectorAll('.hamburger__line');
+    const navLinks = document.querySelector('.header-container__menu');
+
+    navLinks.classList.toggle('header-container__menu-active');
+    hamburgerElements.forEach((element) => {
+        element.classList.toggle('active');
+    })
+}
+
+function changeTheme(event) {
+    const body = document.body;
+    const icon = document.querySelector('.header-container__svg-icon');
+    body.classList.toggle('active-theme');
+
+    if(body.classList.length === 1) {
+        icon.children[0].setAttribute('href', './assets/svg/sprite.svg#moon');
+    } else {
+        icon.children[0].setAttribute('href', './assets/svg/sprite.svg#sun');
+    }
+    
+
+    LIST_CHANGE_BG_COLOR.forEach((element) => {
+        let items = document.querySelectorAll('.' + element);
+    
+        items.forEach((item) => {
+            item.classList.toggle('active-theme');
+        })
+        
+    })
+}
+
+export {getTranslate, changeLang, closeMenu, changeTheme}
--- portfolio/js/index.js
+++ portfolio/js/index.js
@@ -1,36 +1,14 @@
-import i18Obj from './translate.js';
-import getTranslate from './helper.js';
+import {closeMenu, changeLang, changeTheme} from './helper.js';
 
-const navLinks = document.querySelector('.header-container__menu');
+
 const hamburger = document.querySelector('.hamburger');
-const hamburgerElements = document.querySelectorAll('.hamburger__line');
-const buttonSwitch = document.querySelector('.header-container-switch')
-
-
+const buttonSwitch = document.querySelector('.header-container-switch');
+const navLinks = document.querySelector('.header-container__menu');
+const buttonTheme = document.querySelector('.header-container__button');
 
 hamburger.addEventListener('click', closeMenu);
 navLinks.addEventListener('click', closeMenu);
-buttonSwitch.addEventListener('click', changeLang)
-
-function changeLang(event){
-    const activeButton = document.querySelector('.header-container-switch__button_checked');
-   
-    if(!event.target.classList.contains('header-container-switch__button_checked')){
-        event.target.classList.add('header-container-switch__button_checked');
-        activeButton.classList.remove('header-container-switch__button_checked');
-    } 
-
-    getTranslate(event.target.innerHTML);
-}
-
-function closeMenu(event) {
-    navLinks.classList.toggle('header-container__menu-active');
-    hamburgerElements.forEach((element) => {
-        element.classList.toggle('active');
-    })
-}
-
-
-
+buttonSwitch.addEventListener('click', changeLang);
+buttonTheme.addEventListener('click', changeTheme);
 
 //console.log(" Оценка: 85/75 \n 1. Верстка соответствует макету +48 \n 2. Ни на одном из разрешений до 320px включительно не появляется горизонтальная полоса прокрутки +15 \n 3. На ширине экрана 768рх и меньше реализовано адаптивное меню +22")
