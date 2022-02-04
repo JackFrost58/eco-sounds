c222acc50232602db25341fdd6a90b77f1fd8095
JackFrost58 <zgv2324@gmail.com>
2022-02-04 16:42:20 +0300

    feat: add new card bird

--- eco-sounds/js/variables.js
+++ eco-sounds/js/variables.js
@@ -1,4 +1,4 @@
-const ARRAY_NAME_BIRDS = ['Соловей', 'Дрозд', 'Малиновка', 'Жаворонок', 'Славка'];
+const ARRAY_NAME_BIRDS = ['Соловей', 'Дрозд', 'Малиновка', 'Жаворонок', 'Славка', 'Ласточка'];
 
 const DESCRIPTION_BIRDS = {
     'Соловей': {
@@ -40,6 +40,14 @@ const DESCRIPTION_BIRDS = {
         'Класс':'Птицы',
         'Отряд':'Воробьинообразные',
         'Семейство':'Славковые'
+    },
+    'Ласточка': {
+        'Домен':'Эукариоты',
+        'Царство':'Животные',
+        'Тип':'Хордовые',
+        'Класс':'Птицы',
+        'Отряд':'Воробьинообразные',
+        'Семейство':'Ласточковые' 
     }
 }
 
@@ -48,7 +56,8 @@ const LIST_LINKS_BIRDS = {
     'Дрозд': 'https://ru.wikipedia.org/wiki/%D0%9D%D0%B0%D1%81%D1%82%D0%BE%D1%8F%D1%89%D0%B8%D0%B5_%D0%B4%D1%80%D0%BE%D0%B7%D0%B4%D1%8B',
     'Малиновка': 'https://ru.wikipedia.org/wiki/%D0%97%D0%B0%D1%80%D1%8F%D0%BD%D0%BA%D0%B0',
     'Жаворонок': 'https://ru.wikipedia.org/wiki/%D0%96%D0%B0%D0%B2%D0%BE%D1%80%D0%BE%D0%BD%D0%BA%D0%BE%D0%B2%D1%8B%D0%B5',
-    'Славка': 'https://ru.wikipedia.org/wiki/%D0%A1%D0%BB%D0%B0%D0%B2%D0%BA%D0%B8'
+    'Славка': 'https://ru.wikipedia.org/wiki/%D0%A1%D0%BB%D0%B0%D0%B2%D0%BA%D0%B8',
+    'Ласточка': 'https://ru.wikipedia.org/wiki/%D0%94%D0%B5%D1%80%D0%B5%D0%B2%D0%B5%D0%BD%D1%81%D0%BA%D0%B0%D1%8F_%D0%BB%D0%B0%D1%81%D1%82%D0%BE%D1%87%D0%BA%D0%B0'
 }
 
 const TRANSLATE_NAME = {
@@ -57,6 +66,7 @@ const TRANSLATE_NAME = {
     'Малиновка': 'zarynka',
     'Жаворонок': 'javoronok',
     'Славка': 'slavka',
+    'Ласточка': 'lastochka',
     'Лес': 'forest'
 }
 
